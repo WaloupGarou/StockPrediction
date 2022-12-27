4b8f3f2d535e5f8a683e824f3742a89b6b8e4a31
WaloupGarou <arthur_0311@hotmail.fr>
2022-12-27 16:02:50 +0100

    change

--- donnees-fondamentales-historiques.py
+++ donnees-fondamentales-historiques.py
@@ -6,10 +6,22 @@ table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
 df = table[0]
 df = df["Symbol"].to_list()
 
-meta = yf.Ticker("AMZN")
-print(meta.info)
+amazon = yf.Ticker("AMZN")
+balance_sheet = amazon.balance_sheet
+income_statement = amazon.income_stmt
+cash_flow = amazon.cashflow
 
 """
+print("                 #######cashflow############")
+print(cash_flow)
+print("                 #######incomestatement############")
+print(income_statement)
+print("                 #######balance_sheet############")
+print(balance_sheet)
+
+print("                 #######financials############")
+print(amazon.financials)
+
 df = pd.DataFrame.from_dict(dict,orient='index')
 df = df.reset_index().to_csv("test.csv",sep=",")
 
@@ -18,3 +30,6 @@ with open('test.csv', newline='') as f:
     data = list(reader)
 data = [[l[1],l[2]] for l in data ]    
 """
+
+info = amazon.history(period="max")
+print(info["regularMarketEPS"])
